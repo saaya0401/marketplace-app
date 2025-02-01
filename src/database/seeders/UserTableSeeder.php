b74b1a93a3d3cce6017d31ceb8bc73fcea099f22
koba saaya <saaya.koba0401@gmail.com>
2025-02-01 19:44:45 +0900

    トップページ作成

--- src/database/seeders/UserTableSeeder.php
+++ src/database/seeders/UserTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class UserTableSeeder extends Seeder
@@ -14,11 +14,35 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $user=[
+        $users=[
+            [
+            'name'=>'koharu',
+            'email'=>'koharu@example.com',
+            'password'=>Hash::make('koharukoba')
+            ],
+            [
+            'name'=>'hiyori',
+            'email'=>'hiyori@example.com',
+            'password'=>Hash::make('hiyorikoba')
+            ],
+            [
+            'name'=>'yasu',
+            'email'=>'yasu@example.com',
+            'password'=>Hash::make('yasukoba')
+            ],
+            [
+            'name'=>'sarasa',
+            'email'=>'sarasa@example.com',
+            'password'=>Hash::make('sarasakoba')
+            ],
+            [
             'name'=>'saaya',
             'email'=>'saaya@example.com',
             'password'=>Hash::make('saayakoba')
+            ],
         ];
-        DB::table('users')->insert($user);
+        foreach($users as $user){
+            User::create($user);
+        }
     }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,5 +5,25 @@
 @endsection
 
 @section('content')
-
+<div class="tab-area">
+    <div class="tab-buttons">
+        <a href="{{url('/')}}" class="tab-button {{$tab !== 'mylist' ? 'active' : '' }}">おすすめ</a>
+        <a href="{{url('/?tab=mylist')}}" class="tab-button {{$tab === 'mylist' ? 'active' : '' }}">マイリスト</a>
+    </div>
+</div>
+<div class="content">
+    @foreach($items as $item)
+    <form class="item-card__form" action="{{url('/item/' . $item['id'])}}" method="get">
+        @csrf
+        <button class="item-card__button" type="submit">
+            <div class="item-card__image">
+                <img src="{{ Storage::url($item['image'])}}" alt="商品画像" class="item-card__img">
+            </div>
+            <div class="item-card__title">
+                <span class="item-card__name">{{$item['title']}}</span>
+            </div>
+        </button>
+    </form>
+    @endforeach
+</div>
 @endsection
