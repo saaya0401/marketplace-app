1a8031874d61f215ada1dae4f15fca59a4f6216a
koba saaya <saaya.koba0401@gmail.com>
2025-02-03 17:53:42 +0900

    検索機能実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,9 +7,10 @@ use App\Http\Controllers\ItemController;
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::get('/', [ItemController::class, 'index']);
-Route::get('/item/{itemId}', [ItemController::class, 'detail']);
+Route::get('item/search', [ItemController::class, 'search']);
 Route::middleware('auth')->group(function (){
     Route::post('/logout', [UserController::class, 'logout']);
     Route::get('/profile', [UserController::class, 'profile']);
     Route::post('/profile', [UserController::class, 'profileStore']);
 });
+Route::get('/item/{itemId}', [ItemController::class, 'detail']);
