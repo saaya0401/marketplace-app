c0fa44f54e8b21bbb20e197a931119223e4a094a
koba saaya <saaya.koba0401@gmail.com>
2025-03-02 14:35:04 +0900

    mylisttest完成

--- src/tests/Feature/MylistTest.php
+++ src/tests/Feature/MylistTest.php
@@ -14,87 +14,70 @@ use App\Models\Profile;
 class MylistTest extends TestCase
 {
     use RefreshDatabase;
-
+    protected $user;
+    protected $items;
+    protected $mylists;
     public  function setUp(): void
     {
         parent::setUp();
         $this->seed();
+
+        $this->user=User::where('email', 'saaya@example.com')->first();
+        $this->assertNotNull($this->user);
+
+        $this->actingAs($this->user);
+
+        $this->items=Item::all();
+        Mylist::insert([
+            [
+                'user_id'=>$this->user->id,
+                'item_id'=>$this->items[1]->id
+            ],
+            [
+                'user_id'=>$this->user->id,
+                'item_id'=>$this->items[2]->id
+            ],
+            [
+                'user_id'=>$this->user->id,
+                'item_id'=>$this->items[3]->id
+            ]
+        ]);
+        $this->mylists=Mylist::where('user_id', $this->user->id)->get();
     }
 
     public function testMylist(){
-        $user=User::where('email', 'saaya@example.com')->first();
-        $this->assertNotNull($user);
-
-        $this->actingAs($user);
-
-        $items=Item::all();
-        Mylist::insert([
-            [
-                'user_id'=>$user->id,
-                'item_id'=>$items[1]->id,
-            ],
-            [
-                'user_id'=>$user->id,
-                'item_id'=>$items[3]->id,
-            ]
-        ]);
 
         $response=$this->get('/?tab=mylist');
         $response->assertStatus(200);
 
-        $mylists=Mylist::where(
-            'user_id', $user->id)->get();
-        foreach($mylists as $mylist){
+        foreach($this->mylists as $mylist){
             $response->assertSee($mylist->item->title);
             $response->assertSee($mylist->item->image);
         }
     }
 
     public function testSoldMylist(){
-        $user=User::where('email', 'saaya@example.com')->first();
-        $this->assertNotNull($user);
-
-        $this->actingAs($user);
-
         $profiles=Profile::all();
-        $items=Item::all();
-        Mylist::insert([
-            [
-                'user_id'=>$user->id,
-                'item_id'=>$items[1]->id
-            ],
-            [
-                'user_id'=>$user->id,
-                'item_id'=>$items[2]->id
-            ],
-            [
-                'user_id'=>$user->id,
-                'item_id'=>$items[3]->id
-            ]
-            ]);
-
-
-        $mylists=Mylist::whereIn('item_id', [$items[1]->id, $items[2]->id, $items[3]->id])->get();
 
         Purchase::insert([
             [
                 'profile_id'=>$profiles[1]->id,
-                'item_id'=>$items[1]->id,
+                'item_id'=>$this->items[1]->id,
                 'payment_method'=>'カード払い'
             ],
             [
                 'profile_id'=>$profiles[2]->id,
-                'item_id'=>$items[2]->id,
+                'item_id'=>$this->items[2]->id,
                 'payment_method'=>'カード払い'
             ]
         ]);
 
-        $purchases=Purchase::whereIn('item_id', [$items[1]->id, $items[2]->id])->get();
+        $purchases=Purchase::all();
 
         $response=$this->get('/?tab=mylist');
         $response->assertStatus(200);
 
-        foreach($mylists as $mylist){
+        foreach($this->mylists as $mylist){
             $response->assertSee($mylist->item->title);
             $response->assertSee($mylist->item->image);
         }
@@ -102,6 +85,40 @@ class MylistTest extends TestCase
         foreach($purchases as $purchase){
             $response->assertSee($purchase->title);
             $response->assertSee('Sold');
+        }
+    }
+
+    public function testLoginUserMylist(){
+        $userItems=Item::where('user_id', '$this->user->id')->get();
+
+        $response=$this->get('/?tab=mylist');
+        $response->assertStatus(200);
+
+        foreach($this->mylists as $mylist)
+        {
+            $response->assertSee($mylist->item->title);
+            $response->assertSee($mylist->item->image);
+        }
+
+        foreach($userItems as $item)
+        {
+            $response->assertDontSee($item->title);
+            $response->assertDontSee($item->image);
+        }
+    }
+
+    public function testGuestUserMylist(){
+        $response=$this->post('/logout');
+        $this->assertFalse(auth()->check());
+        $response->assertRedirect('/');
+
+        $response=$this->get('/?tab=mylist');
+        $response->assertStatus(200);
+
+        foreach($this->mylists as $mylist)
+        {
+            $response->assertDontSee($mylist->item->title);
+            $response->assertDontSee($mylist->item->image);
         }
     }
 }
